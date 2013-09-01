95ef7dcad11d9f73762c4ed7682f84cb602a8271
Bengt Ericsson <zolomon@gmail.com>
2013-09-01 21:39:01 +0200

    Example GoalRest

--- utils.rs
+++ utils.rs
@@ -52,7 +52,7 @@ pub mod utils {
 	pub enum GoalType {
 		GoalThink,
 		GoalExplore,
-		GoalSleep
+		GoalRest
 	}
 
 	#[deriving(Eq, ToStr)]
@@ -74,10 +74,13 @@ pub mod utils {
 		fn new(owner: @Entity) -> Self;
 		
 		fn activate(&self) 	{ }
-		fn process(&self) 	{ }
+		fn process(&self) -> GoalStatus	{ Inactive }
 		fn terminate(&self)	{ }
 		//fn handle_msg(&self, msg: Telegram) { }
 		fn add_subgoal(&self, goal: @Goal) { }
+		
+		fn activate_if_inactive(&self);
+		fn reactivate_if_failed(&mut self);
 
 		fn is_active(&self) -> bool;
 		fn is_inactive(&self) -> bool;
@@ -86,7 +89,7 @@ pub mod utils {
 		fn get_type(&self) -> GoalType;
 	}
 
-	impl Goal for GoalThink {
+	/*impl Goal for GoalThink {
 		fn new(owner: @Entity) -> GoalThink { 
 			GoalThink { 
 				owner: owner, 
@@ -95,8 +98,50 @@ pub mod utils {
 			}
 		}	
 
-		fn process(&self) {
-			println(fmt!("Goal Think"));
+		fn process(&self) -> GoalStatus {
+			//self.activate_if_inactive();
+		}
+
+		fn is_active(&self) -> bool { self.status == Active }
+		fn is_inactive(&self) -> bool { self.status == Inactive }
+		fn is_completed(&self) -> bool { self.status == Completed }
+		fn has_failed(&self) -> bool { self.status == Failed }
+		fn get_type(&self) -> GoalType { self.goal_type }
+	}*/
+
+	#[deriving(Eq, ToStr)]
+	struct GoalRest {
+		owner: @Entity,
+		status: GoalStatus,
+		goal_type: GoalType
+	}
+
+	impl Goal for GoalRest {
+		fn new(owner: @Entity) -> GoalRest { 
+			GoalRest { 
+				owner: owner, 
+				status: Inactive,
+				goal_type: GoalRest
+			}
+		}	
+
+		fn activate_if_inactive(&self) { if self.is_inactive() { self.activate(); } }
+		fn reactivate_if_failed(&mut self) { if self.has_failed() { self.status = Inactive; } }
+
+		fn activate(&self) {
+			println(fmt!("Activated GoalRest"));
+		}
+
+		fn process(&self) -> GoalStatus {
+			println(fmt!("Processing GoalRest"));
+
+			self.activate_if_inactive();
+
+			return self.status
+		}
+
+		fn terminate(&self) {
+			println(fmt!("Terminating GoalRest"));
 		}
 
 		fn is_active(&self) -> bool { self.status == Active }
@@ -119,11 +164,51 @@ pub mod utils {
 			}
 		}
 
-		fn process(&mut self) {
+		fn terminate(&mut self) {
+			match *self {
+				Node(goal, ref mut children) => for child in children.mut_iter() {
+					child.terminate();
+				},
+				Leaf(goal) => {goal.terminate()}
+			}
+		}
+
+		fn is_completed(&mut self) -> bool {
 			match *self {
 				Node(goal, ref mut children) => {
+					let mut result = false;
+					for child in children.mut_iter() {
+						result |= child.is_completed();
+					}
+					result
+				},
+				Leaf(goal) => { goal.is_completed() }	
+			}			
+		}
 
-					//for child in children.mut_iter() { child.process() }
+		fn process(&mut self) -> GoalStatus {
+			match *self {
+				Node(goal, ref mut children) => {
+					/*while !children.is_empty() && 
+					(children.head().is_completed() || children.head().has_failed()) {
+						children.head().terminate();
+						children.remove(0);												
+					}
+
+					// if any subgoals remain, process the one at the front of the list
+					if !children.is_empty() {
+						// grab the status of the front-most subgoal
+						let status_of_subgoals = children.head().process();
+
+						// we have to test for the special case where the fron-most subgoal
+						// reports "completed" nad the subgoal list contains additional goals.
+						// When this is the case, to ensure the parent keeps processing its
+						// subgoal list, the "active" status is returned
+						if status_of_subgoals == Completed && children.len() > 1 {
+							return Active;
+						}
+					}*/
+					Completed
 				},
 
 				Leaf(goal) => {
